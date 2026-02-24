71c68ee64eb277144058bd061444581b31f2a4d4
hv10 <noeldanz@gmail.com>
2026-02-24 09:58:17 +0100

    test ValTyping for Rule for JuliaC compat

--- src/PassiveAgressive.jl
+++ src/PassiveAgressive.jl
@@ -3,12 +3,16 @@ module PassiveAgressive
 using OnlineStatsBase
 using LinearAlgebra
 
-export PAClassifier, PARegerssor, PAUniclassClassifier
+export PAClassifier, PARegressor, PAUniclassClassifier
 export predict
 
 const Tup{T} = Union{NTuple{N,T} where {N},NamedTuple{names,Tuple{N,<:T} where {N}} where {names}}
 const VectorOb{T} = Union{AbstractVector{<:T},Tup{T}}
 const XY{T,S} = Union{Tuple{T,S},Pair{T,S},NamedTuple{names,Tuple{T,S}}} where {names,T<:VectorOb{Number},S<:Number}
+
+rule(::Val{:base}, lt, x, C) = lt / norm(x)^2
+rule(::Val{:v1}, lt, x, C) = min(C, lt / norm(x)^2)
+rule(::Val{:v2}, lt, x, C) = lt / (norm(x)^2 + 1 / 2 * C)
 
 update_rule(tp::Symbol, C::Number) = begin
     if tp == :base
@@ -24,11 +28,11 @@ end
 mutable struct PAClassifier{F} <: OnlineStat{Union{Tuple,Vector{Number}}}
     weight::Vector{Float64} # weights
     rule::F
+    C::Float64
     n::Int
 end
 PAClassifier(in_::Int=1; type::Symbol=:base, C=1) = begin
-    rule = update_rule(type, C)
-    return PAClassifier(zeros(in_), rule, 0)
+    return PAClassifier(zeros(in_), type, C, 0)
 end
 predict(o::PAClassifier, y::AbstractArray) = sign(dot(o.weight, y))
 OnlineStatsBase._fit!(o::PAClassifier, y::Vector{Number}) = predict(o, y)
@@ -37,9 +41,9 @@ OnlineStatsBase._fit!(o::PAClassifier, y::Tuple) = begin
         yh = predict(o, y[1])
         o.n += 1
         lt = max(0, 1 - y[2] * yh)
-        τ = o.rule(lt, y[1])
+        τ = rule(Val(o.rule), lt, y[1], o.C)
         o.weight = o.weight + τ * y[2] * y[1]
-		return yh
+        return yh
     else
         return predict(o, y[1])
     end
@@ -48,12 +52,12 @@ end
 mutable struct PARegressor{F} <: OnlineStat{Union{Tuple,Vector{Number}}}
     weight::Vector{Float64} # weights
     rule::F
+    C::Float64
     ϵ::Float64
     n::Int
 end
 PARegressor(in_::Int=1; type::Symbol=:base, ϵ=0.1, C=1) = begin
-    rule = update_rule(type, C)
-    return PARegressor(zeros(in_), rule, ϵ, 0)
+    return PARegressor(zeros(in_), type, C, ϵ, 0)
 end
 predict(o::PARegressor, y::AbstractArray) = dot(o.weight, y)
 OnlineStatsBase._fit!(o::PARegressor, y::Vector{Number}) = predict(o, y)
@@ -62,14 +66,18 @@ OnlineStatsBase._fit!(o::PARegressor, y::Tuple) = begin
         yh = predict(o, y[1])
         cond_term = abs(yh − y[2])
         lt = ifelse(cond_term <= o.ϵ, 0, cond_term - o.ϵ)
-        τ = o.rule(lt, y[1])
+        τ = rule(Val(o.rule), lt, y[1], o.C)
         o.weight = o.weight + τ * sign(y[2] - yh) * y[1]
         o.n += 1
-		return yh
+        return yh
     else
         return predict(o, y[1])
     end
 end
+
+rule_uniclass(::Val{:base}, lt, x, C) = lt
+rule_uniclass(::Val{:v1}, lt, x, C) = min(C, lt)
+rule_uniclass(::Val{:v2}, lt, x, C) = lt / (1 + (1 / 2 * C))
 
 update_rule_uniclass(tp::Symbol, C::Number) = begin
     if tp == :base
@@ -85,16 +93,16 @@ end
 """
 I think this is not working correctly.
 """
-mutable struct PAUniclassClassifier{F} <: OnlineStat{AbstractVector{<:Number}}
-    weight::Vector{Float64} # weights
+mutable struct PAUniclassClassifier{T,F} <: OnlineStat{AbstractVector{<:Number}}
+    weight::Vector{T} # weights
     rule::F
+    C::Float64
     ϵ::Float64
     adaptive::Bool
     B::Float64
     n::Int
 end
 PAUniclassClassifier(in_::Int=1; type::Symbol=:base, ϵ=0.1, B=1e10, C=1, adaptive=true) = begin
-    rule = update_rule_uniclass(type, C)
     if adaptive
         ϵ = 0.0 # init ϵ to be zero
         weight = zeros(in_ + 1)
@@ -102,9 +110,9 @@ PAUniclassClassifier(in_::Int=1; type::Symbol=:base, ϵ=0.1, B=1e10, C=1, adapti
     else
         weight = zeros(in_)
     end
-    return PAUniclassClassifier(weight, rule, ϵ, adaptive, B, 0)
+    return PAUniclassClassifier(weight, type, C, ϵ, adaptive, B, 0)
 end
-predict(o::PAUniclassClassifier, y::AbstractVector{<:Number}) = o.adaptive ? dot(o.weight[1:end-1], y) : dot(o.weight, y)
+predict(o::PAUniclassClassifier{T}, y::AbstractVector{T}) where T = o.adaptive ? dot(o.weight[1:end-1], y) : dot(o.weight, y)
 OnlineStatsBase._fit!(o::PAUniclassClassifier, y::AbstractVector{<:Number}) = begin
     w = o.weight
     if o.adaptive
@@ -116,7 +124,7 @@ OnlineStatsBase._fit!(o::PAUniclassClassifier, y::AbstractVector{<:Number}) = be
     else
         lt = ifelse(cond_term <= o.ϵ, 0, cond_term - o.ϵ)
     end
-    τ = o.rule(lt, y)
+    τ = rule_uniclass(Val(o.rule), lt, y, o.C)
     w = w + τ * ((y - w) / norm(y - w, 2))
     if o.adaptive
         # update our bound ϵ
@@ -125,11 +133,11 @@ OnlineStatsBase._fit!(o::PAUniclassClassifier, y::AbstractVector{<:Number}) = be
     end
     o.weight = w
     o.n += 1
-	if o.adaptive
-		return predict(o, y[1:end-1])
-	else
-		return predict(o, y)
-	end
+    if o.adaptive
+        return predict(o, y[1:end-1])
+    else
+        return predict(o, y)
+    end
 end
 
 end # module PassiveAgressive
